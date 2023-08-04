be5a6dedf2e41115a8e41de764a318c14cf97114
USII-004 <usmansmooth004@gmail.com>
2023-08-04 03:44:29 +0100

    tesing out reactDOM and render

---
+++ simpleReactPage/index.js
@@ -0,0 +1,59 @@
+function navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+      aria-expanded="false" aria-label="toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another Action</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  )
+}
+
+
+ReactDOM.render(<h1>Hello, this is my first using React</h1>, document.getElementById('root'))
+
+ReactDOM.render(<p>This is pretty <strong>amaizing</strong> the power <i>React</i> has.</p>,
+                document.getElementById('subtext'))
+
+ReactDOM.render(<h3>List of things I have learnt so far!</h3>, document.getElementById('subtitle'))
+
+ReactDOM.render(<ul>
+                  <li>ReactDom</li>
+                  <li>render</li>
+                </ul>, document.getElementById('list'))
+
